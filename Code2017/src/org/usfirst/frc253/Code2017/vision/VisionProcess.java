e4d396f3dd43b8ff3866aca50c65801feac1685a
xiaoeric <alpha2275@gmail.com>
2017-10-10 00:02:13 -0700

    dcafdvs

--- Code2017/src/org/usfirst/frc253/Code2017/vision/VisionProcess.java
+++ Code2017/src/org/usfirst/frc253/Code2017/vision/VisionProcess.java
@@ -3,12 +3,15 @@ package org.usfirst.frc253.Code2017.vision;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc253.Code2017.Robot;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.vision.VisionThread;
 
 
 public class VisionProcess extends Thread {
 	private VisionThread visionThread;
 	private final Object imgLock = new Object();
+	private AnalogGyro gyro;
 	
 	//Constants
 	private final double realHeight = 2; //TODO find actual height
@@ -17,6 +20,8 @@ public class VisionProcess extends Thread {
 	private final double FOV = 60;
     private final double horizontalDPP = FOV/Robot.CAMERA_WIDTH;
     private final double cameraCenter = Robot.CAMERA_WIDTH/2;
+    private final double leftPegBearing = 0; //TODO find value
+    private final double rightPegBearing = 0; //TODO find value
 
 	//Intermediate calculations
 	//Numbers that are important but only because we need them to calculate other things
@@ -26,11 +31,13 @@ public class VisionProcess extends Thread {
 	private double angleFromPeg = 0.0;
 	private double distanceDirect = 0.0;
 	private double angleRobot = 0.0;
+	private double robotBearing = 0.0;
 	
 	private double distanceOffset = 0.0;
 	private double distanceTravel = 0.0;
     
     public VisionProcess() {
+    	this.gyro = Robot.sensorData.gyro;
     	visionThread = new VisionThread(Robot.camera, new GripPipeline(), pipeline -> {
             if (!pipeline.filterContoursOutput().isEmpty()) {
                 Rect peg = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
@@ -38,9 +45,16 @@ public class VisionProcess extends Thread {
                     pegCenter = peg.x + (peg.width / 2);
                     //robot facing to the right of the peg is positive
                     angleRobot = ((cameraCenter - pegCenter) * horizontalDPP) * (Math.PI / 180);
+                    //comparing real and perceived height to calculate distance
                     perceivedHeight = peg.height;
                     distanceDirect = (realHeight * focalLength)/perceivedHeight;
-                    angleFromPeg = (Math.PI/2) - Math.asin(peg.width / realWidth);
+                    //DEPRECATED find angle from peg
+                    robotBearing = (gyro.getAngle() % 360) * (Math.PI/180);
+                    /*
+                     * find legs of the right triangle formed by the peg
+                     * and the robot; offset is x and travel is y if peg
+                     * is uppermost point of triangle
+                     */
                     distanceOffset = distanceDirect * Math.sin(angleFromPeg);
                     distanceTravel = distanceDirect * Math.cos(angleFromPeg);
                 }
@@ -49,6 +63,7 @@ public class VisionProcess extends Thread {
     }
     @Override
     public void run() {
+    	this.gyro.reset();
     	visionThread.start();
     }
     
@@ -63,6 +78,19 @@ public class VisionProcess extends Thread {
 				{distanceTravel, distanceOffset}
     		}; 
     		return waypoints;
+    	}
+    }
+    
+    /**
+     * @return 1 = left; 2 = center; 3 = right
+     */
+    public int whichPeg() {
+    	int output;
+    	synchronized (imgLock) {
+    		if(Math.abs(robotBearing - angleRobot) < (2.0 * (Math.PI/180))) {
+    			return 2;
+    		}
+    		return 0; //TODO add conditionals for left and right
     	}
     }
 }
